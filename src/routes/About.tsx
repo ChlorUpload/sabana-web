3ed94c2cb0a2a956411df65cb6ad72cdfce1e088
chlorUpload <chlor.upload@gmail.com>
2021-11-29 21:55:51 +0900

    테스트용 AutoScrollViewer 렌더

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,11 +1,17 @@
 import { TopNavigation } from "componenets/About/TopNavigation";
 import Footer from "componenets/Footer";
 import Vertical from "componenets/Vertical";
+import { AutoScrollViewer } from "sabana-music";
 
 export default function About() {
   return (
     <Vertical>
       <TopNavigation></TopNavigation>
+      <AutoScrollViewer
+        sheetKey="sheet-key"
+        title="AutoScrollViewer"
+        url="https://opensheetmusicdisplay.github.io/demo/sheets/MuzioClementi_SonatinaOpus36No3_Part1.xml"
+      ></AutoScrollViewer>
       <Footer></Footer>
     </Vertical>
   );
