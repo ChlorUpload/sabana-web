f56c8220c3ada4d37c9c5607a159c17e77301170
chlorUpload <chlor.upload@gmail.com>
2021-11-29 21:55:00 +0900

    푸터 컴포넌트 추가

--- src/componenets/Footer.tsx
+++ src/componenets/Footer.tsx
@@ -1,3 +1,10 @@
+import styled from "styled-components";
+
+const Cont = styled.div`
+  width: 100%;
+  background-color: #ebebeb;
+`;
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return <Cont>Footer</Cont>;
 }
