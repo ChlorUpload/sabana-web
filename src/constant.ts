8a4973f171327ce7479ccd9e75fe0cb79d2d73ab
chlorUpload <chlor.upload@gmail.com>
2021-11-29 21:54:53 +0900

    상수 추가

---
+++ src/constant.ts
@@ -0,0 +1,11 @@
+const DESKTOP_WIDTH = 1030;
+const TABLET_WIDTH = 724;
+const MARGIN = 20;
+const HORIZONTAL_MARGIN = 50;
+
+export const size = {
+  margin: MARGIN,
+  maxWidth: DESKTOP_WIDTH,
+  desktopMin: DESKTOP_WIDTH + 2 * HORIZONTAL_MARGIN,
+  tabletMin: TABLET_WIDTH,
+};
