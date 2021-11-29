1e30eed8344460d9a30e310a19d6e6d58eff41fb
chlorUpload <chlor.upload@gmail.com>
2021-11-29 20:20:11 +0900

    sabana-web 추가

---
+++ src/componenets/Vertical.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Vertical = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Vertical;
---
+++ src/routes/About.tsx
@@ -0,0 +1,12 @@
+import { TopNavigation } from "componenets/About/TopNavigation";
+import Footer from "componenets/Footer";
+import Vertical from "componenets/Vertical";
+
+export default function About() {
+  return (
+    <Vertical>
+      <TopNavigation></TopNavigation>
+      <Footer></Footer>
+    </Vertical>
+  );
+}
